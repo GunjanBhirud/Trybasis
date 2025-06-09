47a93a517671e57b095a53db5dab1d84e78b23e6
gun <gunj@gmail.com>
2025-06-09 14:18:15 +0530

    remove

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Realestate.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
