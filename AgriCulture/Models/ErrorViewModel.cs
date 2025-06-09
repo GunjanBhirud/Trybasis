e10cde1e7b98fdb9a287b65f907d73e014a99cc5
gunj <gunj@gmail.com>
2025-06-09 10:39:42 +0530

    unwanted

--- AgriCulture/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AgriCulture.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
